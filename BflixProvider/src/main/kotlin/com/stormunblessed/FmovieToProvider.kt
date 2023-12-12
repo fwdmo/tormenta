4cb5691a7ba6160d7b3ebe80ff8b8539c2551273
sudo@x0.tn <27734703+fwdmo@users.noreply.github.com>
2023-12-12 13:37:28 +0100

    Update FmovieToProvider.kt

--- BflixProvider/src/main/kotlin/com/stormunblessed/FmovieToProvider.kt
+++ BflixProvider/src/main/kotlin/com/stormunblessed/FmovieToProvider.kt
@@ -2,5 +2,5 @@ package com.stormunblessed
 
 class FmoviesToProvider : BflixProvider() {
     override var mainUrl = "https://fmoviesz.to"
-    override var name = "Fmovies.to"
+    override var name = "Fmoviesz.to (Fmovies.to)"
 }
